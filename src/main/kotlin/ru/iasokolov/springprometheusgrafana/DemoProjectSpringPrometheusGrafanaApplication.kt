7a31557369d2336200a8e3d80b796e038aed0c9a
Sokolov Ilya <iasokolov@gmail.com>
2023-10-01 10:45:17 +0300

    Init Spring Boot

---
+++ src/main/kotlin/ru/iasokolov/springprometheusgrafana/DemoProjectSpringPrometheusGrafanaApplication.kt
@@ -0,0 +1,11 @@
+package ru.iasokolov.springprometheusgrafana
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoProjectSpringPrometheusGrafanaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoProjectSpringPrometheusGrafanaApplication>(*args)
+}
---
+++ src/test/kotlin/ru/iasokolov/springprometheusgrafana/DemoProjectSpringPrometheusGrafanaApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.iasokolov.springprometheusgrafana
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoProjectSpringPrometheusGrafanaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
